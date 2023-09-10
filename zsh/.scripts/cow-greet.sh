d56490cae5519b774277ce6e69c1409a8d693889
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2023-09-10 19:47:09 -0400

    feat: walter

--- zsh/.scripts/cow-greet.sh
+++ zsh/.scripts/cow-greet.sh
@@ -2,7 +2,7 @@ cowsay_commands=(
     "cowsay -f ~/.cowfiles/finn-and-jake.cow 'you are \"sed\"istic'"
     "cowsay -f ~/.cowfiles/alfred.cow 'Welcome home, sir. Shall I fetch the runbook?'"
     "cowsay -f ~/.cowfiles/sparrow.cow '200?'"
-    "cowsay -f ~/.cowfiles/alfred.cow 'I am the one who awks'"
+    "cowsay -f ~/.cowfiles/walter.cow 'I am the one who awks'"
 )
 
 random_index=$((RANDOM % ${#cowsay_commands[@]}))
